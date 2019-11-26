9b0247632326fbef4bd6af7c138d4f5966b82656
TakoNya <camilabelentaraborelli2@gmail.com>
2019-11-25 21:56:19 -0300

    MuestraNumWhile

---
+++ src/muestranumwhileapp/MuestraNumWhileApp.java
@@ -0,0 +1,22 @@
+package muestranumwhileapp;
+
+public class MuestraNumWhileApp {
+
+        public static void main(String[] args) {
+        // Inicializar contador.
+        int num = 1;
+        
+        //Mientras contador sea menor o igual al 100.
+        while(num <= 100){
+            //Mostramos el contador.
+            System.out.println(num);
+            
+            //Incrementamos contador.
+            num++;
+            
+        }
+        
+        
+    }
+    
+}
